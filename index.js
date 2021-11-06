404d444cfabcdd0ed210639a4299c2e224d63966
RaeWinTan <raewintan@gmail.com>
2021-11-06 12:32:19 +0800

    port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config({ path: './config.env' });
-const PORT = 8000;
+const PORT = 443;
 const axios = require('axios');
 const express = require('express');
 var bodyParser = require('body-parser');

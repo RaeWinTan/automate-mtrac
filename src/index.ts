a031a0e157130cd7599356cc084835830811f98c
RaeWinTan <raewintan@gmail.com>
2022-02-02 22:24:19 +0800

    more unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const PORT = 8000;
 import express from 'express'
 import {mtracRouter} from "./routes/mtrac.router";
 import bodyParser from 'body-parser';
-import session from "express-session";
+
 const app = express();
 app.use(bodyParser.json());
 app.use("/api/mtrac", mtracRouter);
